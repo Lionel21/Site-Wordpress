93a355e52d88ca4d09ec0789cd3c03402e0ad055
Lionel Delamare <lionel.delamare>
2021-03-08 16:28:10 +0100

    Bootstrap setted up

--- wp-content/themes/MonTheme/functions.php
+++ wp-content/themes/MonTheme/functions.php
@@ -1,5 +1,19 @@
 <?php
 
-add_theme_support('title-tag');
+namespace App;
+
+function myTheme_support() {
+    add_theme_support('title-tag');
+}
+
+function myTheme_register_assets() {
+    wp_register_style('Bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css');
+    wp_enqueue_style('Bootstrap');
+}
+
+
+
+add_action('after_setup_theme', 'App\myTheme_support');
+add_action('wp_enqueue_scripts', 'App\myTheme_register_assets');
 
 ?>
